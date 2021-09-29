33a700f3bd659d938e281ab333dee21f3968d9b2
karina leal <89037437+Kakaleal@users.noreply.github.com>
2021-09-29 09:08:32 -0300

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+public class Casting {
+	public static void main(String[] args) {
+		int a = 10;
+		int b=2;
+		int soma= a + b;
+		
+		System.out.println(" A soma é : "+ soma);
+		
+		double divisao= ((double)a  + (double) b )/2;
+		System.out.println("A divisão é : " + divisao );
+	}
+
+}
